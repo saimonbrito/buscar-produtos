a9ee8acdea113669179992c9e1eb2f73f09882a5
saimonBrito <britomauricio89@gmail.com>
2023-12-26 22:55:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+class Produto{
+
+  constructor(){
+    this.id = 0;
+    this.nomeProdutos = " ";
+    this.valor = 0;
+  };
+
+   adicionar(){
+    alert('tamos junto')
+   }
+
+   excluir(){
+    alert('Item deletado!');
+  }
+};
+
+const produto = new Produto();
